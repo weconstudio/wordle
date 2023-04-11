3ef4fb6c88a0462cb6df7f417be4b8045fbc20ff
Donald Shtjefni <dnld.sht@gmail.com>
2023-04-11 10:17:36 +0200

    feat: step-0

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      absolute
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          Scrivi il tuo nome
+        </v-card-title>
+        <v-card-text>
+          <v-text-field filled label="Nome" v-model="name"></v-text-field>
+          <v-divider class="mb-4"></v-divider>
+          <template v-if="name">
+            <h1>CIAO {{ nameUppercase }}!</h1>
+            <v-btn class="mt-4" color="primary" @click="reset">Resetta</v-btn>
+          </template>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: ''
+  }),
+  computed: {
+    nameUppercase() {
+      return this.name.toUpperCase()
+    }
+  },
+  methods: {
+    reset() {
+      this.name = ''
+    },
+  }
+}
+</script>
