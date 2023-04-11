00d0250daa614778426374e5eb0298f7f2b15552
Donald Shtjefni <dnld.sht@gmail.com>
2023-04-11 11:39:49 +0200

    feat: step-2

---
+++ components/Guess.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- Usare il componente Char per mostrare i caratteri di guess (computed characters) -->
+</template>
+<script>
+export default {
+  props: {
+    guess: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    characters() {
+      // TODO: tornare l'array dei caratteri di this.guess
+
+      return ['a', 'b', 'c', 'd', 'e']
+    },
+  }
+}
+</script>
