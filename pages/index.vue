941895a12b0bb06df784c2065a4b67981e421207
Alessandro Cabutto <alessandro.cabutto@gmail.com>
2023-04-17 08:34:54 +0200

    piccole rifiniture

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,12 @@
     <v-col cols="12" sm="8" md="6">
       <v-card>
         <v-card-title class="headline">
-          Wordle
+          Step 1
         </v-card-title>
+        <v-card-subtitle>
+          Inserisci una parola nella casella di testo e premi il tasto INVIA. Cosa succede se la parola è troppo lunga?
+        </v-card-subtitle>
         <v-card-text>
-
           <div class="word">
             <Char v-for="(char,i) in characters" :key="i" :char="char" />
           </div>
@@ -16,23 +18,15 @@
     </v-col>
   </v-row>
 </template>
-<style lang="scss">
-.word {
-  display: flex;
-  gap: 10px;
-}
-</style>
 
 <script>
-
 export default {
   data: () => ({
-    
     characters: ['', '', '', '', ''],
   }),
-  
+
   computed: {
-    
+
   },
   methods: {
     handleGuess(guess) {
@@ -41,3 +35,10 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.word {
+  display: flex;
+  gap: 10px;
+}
+</style>
