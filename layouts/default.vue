6cdc7d78653bc671aa3f5ab574a5a06ce05fc194
Alessandro Cabutto <alessandro.cabutto@gmail.com>
2023-04-17 08:43:27 +0200

    pulizia

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,15 @@
 <template>
-  <v-app dark>
+  <v-app>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer
-      absolute
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
-  
+
 }
 </script>
